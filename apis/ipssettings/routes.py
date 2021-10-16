13da9a114a68533d760d3be64bc8f673ed4c2b61
Tung HOANG <tung.hoang@gmail.com>
2021-10-16 16:02:47 +0700

    add ipssettings 2

---
+++ apis/ipssettings/routes.py
@@ -0,0 +1,22 @@
+from flask_restplus import Resource
+from .db import *
+
+def init_routes(api, model):
+  @api.route('/')
+  class ListInstances(Resource):
+    @api.doc('find setting param')
+    @api.expect(model)
+    @api.marshal_list_with(model)
+    def put(self):
+      '''find setting param'''
+      return findSettings(api.payload)
+
+  @api.route('/<int:id>')
+  class Instance(Resource):
+    @api.doc('update setting param', body=model)
+    @api.expect(model)
+    @api.marshal_with(model)
+    def put(self, id):
+      '''update setting params'''
+      return updateSettings(id, api.payload)
+    pass
